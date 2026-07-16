097233317d6723ce553cb537c7f5e58f2dbed9df
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-17 02:55:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const { Client, GatewayIntentBits, ChannelType } = require("discord.js");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+});
+
+const CREATE_CHANNEL_ID = "1354771746221461617";
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+});
+
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  if (newState.channelId === CREATE_CHANNEL_ID) {
+    const channel = await newState.guild.channels.create({
+      name: `${newState.member.user.username}'s VC`,
+      type: ChannelType.GuildVoice,
+      parent: newState.channel.parentId,
+    });
+
+    await newState.setChannel(channel);
+  }
+
+  if (
+    oldState.channel &&
+    oldState.channel.members.size === 0 &&
+    oldState.channel.name.endsWith("'s VC")
+  ) {
+    await oldState.channel.delete().catch(() => {});
+  }
+});
+
+client.login(process.env.TOKEN);
